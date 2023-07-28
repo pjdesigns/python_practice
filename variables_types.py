9cd3866aa98f765685436cae2d91bc91cac91809
Phillip Johnson <jp2127@gmail.com>
2023-07-28 18:16:20 -0400

    Add files via upload

---
+++ variables_types.py
@@ -0,0 +1,28 @@
+# Variable and Types
+
+
+# String
+name = 'phillip'
+# Number (Integer)
+age= 32
+print(name, age)
+
+balance = 35000
+
+print(f"Hello your name is {name} and you are {age} old")
+
+
+# Calculating BMI
+
+height = 1.79
+weight = 68.7
+
+#Another way to write this code is bmi = weight / height **2
+bmi = weight / (height * height)
+    
+print(bmi)
+
+
+# Discovering Types
+print(type(name))
+print(type(age))
